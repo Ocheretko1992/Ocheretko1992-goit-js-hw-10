f881ada90b543127371eb6cd6c78a9565e4c6f26
Ocheretko1992 <117608037+Ocheretko1992@users.noreply.github.com>
2024-03-27 20:16:29 +0200

    com 2

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,7 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 let userSelectedDate;
 let timeInterval;
